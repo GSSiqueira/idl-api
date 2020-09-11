d63b6a6d4400763e3ff7d3640ee24ea60186d53e
Gabriel Siqueira <gssiqueira92@gmail.com>
2020-09-11 18:56:53 -0300

    Added ESLint and Prettier plugins

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
-});
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello Young World!' })
+);
 
 app.listen(3030);
